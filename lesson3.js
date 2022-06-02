b8948bbbc2cabf87f47482e6dbb60c29d8af7ebf
hanieith <shkutkoff@gmail.com>
2022-06-02 19:25:01 +0300

    02.06

---
+++ lesson3.js
@@ -0,0 +1,14 @@
+console.log(typeof a);
+let c = 1/0;
+console.log(typeof c);
+let b = 'string'/2;
+console.log(typeof b);
+console.log(`Count: ${b}`);
+console.log(`Count: ${1 + 2}`);
+let msg1 = "строка \"привет\"";
+console.log(msg1);
+console.log(4 > 1);
+let idProcess = null;
+console.log(idProcess);
+let arg;
+console.log(arg);
